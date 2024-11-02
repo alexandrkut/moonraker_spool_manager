c9a0fe205e0dddce56c314bea33d3611f18196a2
alexandrkut <32929279+alexandrkut@users.noreply.github.com>
2024-11-02 13:41:22 +0300

    Update spool_event.py
    Добавлен адрес веб сервера с базой
    Исправлена совместимость запросов (wget -b не везде работает).

--- klipper/moonraker/moonraker/components/spool_event.py
+++ klipper/moonraker/moonraker/components/spool_event.py
@@ -20,8 +20,10 @@ class SpoolEvent:
         self.server = config.get_server()
         global printer_serial 
         global printer_port 
+        global sm_db_host
         printer_serial = config.get('printer_serial')
         printer_port = config.get('moonraker_port')
+        sm_db_host = config.get('sm_db_host')
         self.server.register_event_handler(
             "history:history_changed", self._history_change)
 
@@ -33,34 +35,40 @@ class SpoolEvent:
             filament_used = job_data['filament_used']
             filename = job_data['filename']
             data =  {'printer_serial':printer_serial,
-	            'lenght':filament_used,
-	            'filename':filename,
-	            'printer_port':str(printer_port)}
-            requests.post('http://localhost/sm/put_job.php', data = data) 
+                'lenght':filament_used,
+                'filename':filename,
+                'printer_port':str(printer_port)}
+            requests.post('http://'+sm_db_host+'/sm/put_job.php', data = data) 
             #Возврат потока на 100% по окончанию печати
-            run_command = "wget -b -q -O /dev/null http://localhost:"+printer_port+"/printer/gcode/script?script=M221%20S100"
+            run_command = "curl -s -o /dev/null http://localhost:"+printer_port+"/printer/gcode/script?script=M221%20S100 &"
             os.system(run_command)
-            run_command = "wget -b -q -O /dev/null http://localhost:"+printer_port+"/printer/gcode/script?script=SET_RETRACTION%20RETRACT_LENGTH=0%20RETRACT_SPEED=10%20UNRETRACT_EXTRA_LENGTH=0%20UNRETRACT_SPEED=10"
+            run_command = "curl -s -o /dev/null http://localhost:"+printer_port+"/printer/gcode/script?script=SET_RETRACTION%20RETRACT_LENGTH=0%20RETRACT_SPEED=10%20UNRETRACT_EXTRA_LENGTH=0%20UNRETRACT_SPEED=10 &"
             os.system(run_command)
+            run_command = "curl -s -o /dev/null http://localhost:"+printer_port+"/printer/gcode/script?script=SET_PRESSURE_ADVANCE%20ADVANCE=0.00 &"
+            os.system(run_command)
+
             logging.info(f"catch job finished - {printer_serial} - {filament_used} - {filename}")
         
         elif action == 'added':
-    	    data =  {'printer_serial':printer_serial,
+            data =  {'printer_serial':printer_serial,
 		    'printer_port':str(printer_port)}
-    	    spool_param_json = requests.post('http://localhost/sm/get_spool_param.php', data = data)
-    	    spool_param = json.loads(spool_param_json.text)
-    	    flow = str(spool_param.get('flow'))
-    	    RetLen = str(spool_param.get('RetLen'))
-    	    RetSp = str(spool_param.get('RetSp'))
-    	    UnRetExtrLen = str(spool_param.get('UnRetExtrLen'))
-    	    UnRetSp = str(spool_param.get('UnRetSp'))
-     	    #установка потока из базы при начале печати
-    	    run_command = "wget -b -q -O /dev/null http://localhost:"+printer_port+"/printer/gcode/script?script=M221%20S"+flow
-    	    os.system(run_command)
-    	    #установка ретрактов из базы при начале печати
-    	    run_command = "wget -b -q -O /dev/null http://localhost:"+printer_port+"/printer/gcode/script?script=SET_RETRACTION%20RETRACT_LENGTH="+RetLen+"%20RETRACT_SPEED="+RetSp+"%20UNRETRACT_EXTRA_LENGTH="+UnRetExtrLen+"%20UNRETRACT_SPEED="+UnRetSp
-    	    os.system(run_command)
-    	    logging.info(f"catch job started - {printer_serial} - {printer_port} - {flow}")
+            spool_param_json = requests.post('http://'+sm_db_host+'/sm/get_spool_param.php', data = data)
+            spool_param = json.loads(spool_param_json.text)
+            flow = str(spool_param.get('flow'))
+            RetLen = str(spool_param.get('RetLen'))
+            RetSp = str(spool_param.get('RetSp'))
+            UnRetExtrLen = str(spool_param.get('UnRetExtrLen'))
+            UnRetSp = str(spool_param.get('UnRetSp'))
+            PA = str(spool_param.get('PA'))
+            #установка потока из базы при начале печати
+            run_command = "curl -s -o /dev/null http://localhost:"+printer_port+"/printer/gcode/script?script=M221%20S"+flow+"&"
+            os.system(run_command)
+            #установка ретрактов из базы при начале печати
+            run_command = "curl -s -o /dev/null http://localhost:"+printer_port+"/printer/gcode/script?script=SET_RETRACTION%20RETRACT_LENGTH="+RetLen+"%20RETRACT_SPEED="+RetSp+"%20UNRETRACT_EXTRA_LENGTH="+UnRetExtrLen+"%20UNRETRACT_SPEED="+UnRetSp+"&"
+            os.system(run_command)
+            run_command = "curl -s -o /dev/null http://localhost:"+printer_port+"/printer/gcode/script?script=SET_PRESSURE_ADVANCE%20ADVANCE="+PA+"&"
+            os.system(run_command)
+            logging.info(f"catch job started - {printer_serial} - {printer_port} - {flow} - "+PA)
 
 
 def load_component(config: ConfigHelper) -> SpoolEvent:
